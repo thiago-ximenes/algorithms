90405055472c77530126b5bbef16a4c5244ae5fb
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-09-04 07:35:35 -0300

    pass: pass requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if target_time is None or not isinstance(
+        permanence_period, (list, tuple, int)
+    ):
+        return None
+
+    counter = 0
+    for start_time, end_time in permanence_period:
+        if not isinstance(start_time, int) or not isinstance(end_time, int):
+            return None
+        elif start_time <= target_time <= end_time:
+            counter += 1
+
+    return counter
