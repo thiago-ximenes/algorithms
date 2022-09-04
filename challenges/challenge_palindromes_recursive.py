cdf8c5ce9daa403b87613b59198b7b746edd328a
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-09-04 08:29:06 -0300

    pass: pass requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if word == "":
+        return False
+
+    word_length = len(word)
+
+    if word_length <= 1:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return (
+            True
+            if word[1: word_length - 1] == ""
+            else is_palindrome_recursive(
+                word[1: word_length - 1], 0, high_index - 2
+            )
+        )
+    else:
+        return False
